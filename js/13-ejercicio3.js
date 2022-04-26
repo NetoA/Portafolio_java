1970b28c409eccb34d58318bc90eb160aa05055e
NetoA <ernestoaguirreco@hotmail.com>
2022-04-25 21:13:30 -0500

    ejercicio3a

--- js/13-ejercicio3.js
+++ js/13-ejercicio3.js
@@ -1,10 +1,11 @@
 'use strict'
 
-var numero1=parseInt(prompt("digite el primer numero"));
-var numero2=parseInt(prompt("digite el segundo numero"));
+var numero1=parseInt(prompt("digite el primer numero",0));
+var numero2=parseInt(prompt("digite el segundo numero",0));
 
+document.write("<h1>El numero esta entre " + numero1 + " y " + numero2 + "</h1>");
 for(var i=numero1; i<numero2; i++) {
     if (i>numero1){
-    console.log("El numero esta entre " + numero1 + " y " + numero2 + " es " + i);
+    document.write("El numero esta entre " + numero1 + " y " + numero2 + " es " + i + "</br>");
     }
 }
