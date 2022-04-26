75cb9f0f333f51803873665ba2133fda00fe1942
NetoA <ernestoaguirreco@hotmail.com>
2022-04-26 18:01:19 -0500

    ejercicio4 numeros impares entre 2 numeros

--- js/14-ejercicio4.js
+++ js/14-ejercicio4.js
@@ -5,8 +5,8 @@ var numero2=parseInt(prompt("digite el segundo numero",0));
 
 document.write("<h1>El numero esta entre " + numero1 + " y " + numero2 + "</h1>");
 for(var i=numero1; i<numero2; i++) {
-    if (i>numero1){
-    document.write("El numero esta entre " + numero1 + " y " + numero2 + " es " + i + "</br>");
-    
+    if (i>numero1 && i % 2 >0){
+    document.write("El numero non que esta entre " + numero1 + " y " + numero2 + " es " + i + "</br>");
+    document.write(i % 2 + "</br>");
     }
 }
