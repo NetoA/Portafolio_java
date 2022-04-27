400f8e3d2a74bbe5b57ce7073a02139c4aa36111
NetoA <ernestoaguirreco@hotmail.com>
2022-04-26 20:34:14 -0500

    ejercicio7 tabla de multiplicar

---
+++ js/17-ejercicio7.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var numero=parseInt(prompt("Numero del cual desea la tabla",0))
+
+for(var i=0; i<=10;i++){
+    document.write(i + " x " + numero + " = " + i * numero + "</br>");
+}
