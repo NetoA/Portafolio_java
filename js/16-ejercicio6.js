ec643791739827a2bcbaa1c1b435e5a794af23f4
NetoA <ernestoaguirreco@hotmail.com>
2022-04-26 20:15:33 -0500

    ejercicio6 cuando un numero es par o impar

---
+++ js/16-ejercicio6.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+do{
+var numero=parseInt(prompt("Digite el numero que desa",0));
+
+    if(numero % 2 >0){
+        alert(numero + " es un numero non");
+    }else{
+        alert(numero + " es un numero par");
+    }
+}
+while(isNaN(numero))
